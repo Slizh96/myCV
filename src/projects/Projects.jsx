ec3939f1669e65ad130d9d7c220d2b439fa540d0
Slizh96 <slizh.olya@mail.ru>
2022-09-20 01:34:25 +0300

    component my projects ready

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -3,16 +3,24 @@ import s from './Projects.module.css'
 import c from '../common/styles/Container.module.css'
 import {Project} from "./project/Project";
 import {Title} from "../common/components/Title/Title";
+import todoImg from '../assets/image/todo.jpg';
+import socImg from '../assets/image/soc.png'
 
 export const Projects = () => {
+    const social = {
+        backgroundImage: `url(${socImg})`,
+    };
+    const todo = {
+        backgroundImage: `url(${todoImg})`,
+    };
     return (
         <div className={s.projectsBlock}>
             <div className={`${c.container} ${s.projectsContainer}`}> {/*второй частью перезатираем нужные параметры*/}
                 <Title title='My projects'/>
                 <div className={s.projects}>
-                    <Project src='' title='project 1'
+                    <Project style={social} src='' title='Social network'
                              description='i use js, ts, css, html, react, redux, in this project'/>
-                    <Project src='' title='project 2'
+                    <Project style={todo} src='' title='Todo list'
                              description='i use ALSO js, ts, css, html, react, redux, in this project'/>
                 </div>
             </div>
